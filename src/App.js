919f1a8e62e3c26949802ef09a58a630778f71b7
Denise <deniselewis1120@gmail.com>
2023-11-02 13:37:54 -0700

    Create buttons for user input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [userChoice, setUserChoice] = useState("");
+  const choices = ["rock", "paper", "scissors"];
+
+  const handleClick = value => setUserChoice(value);
+
   return (
     <div>
+      <h1>User choice is: {userChoice}</h1>
+      <h1>Computer choice is: </h1>
+      {choices.map((choice, index) => <button key={index} onClick={() => handleClick(choice)}>{choice}</button>)}
     </div>
   );
 }
