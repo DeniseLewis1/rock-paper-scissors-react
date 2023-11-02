a471bc82fdad99a32e8f0f3b2e34956f439105ff
Denise <deniselewis1120@gmail.com>
2023-11-02 13:44:34 -0700

    Create function to generate computer choice

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import { useState } from 'react';
 
 const App = () => {
   const [userChoice, setUserChoice] = useState("");
+  const [computerChoice, setComputerChoice] = useState("");
   const choices = ["rock", "paper", "scissors"];
 
-  const handleClick = value => setUserChoice(value);
+  const handleClick = value => {
+    setUserChoice(value);
+    generateComputerChoice();
+  };
+
+  const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+  };
 
   return (
     <div>
       <h1>User choice is: {userChoice}</h1>
-      <h1>Computer choice is: </h1>
+      <h1>Computer choice is: {computerChoice}</h1>
       {choices.map((choice, index) => <button key={index} onClick={() => handleClick(choice)}>{choice}</button>)}
     </div>
   );
