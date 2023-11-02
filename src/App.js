e8ce3aecad0eefda69f232f71e733e5497038ece
Denise <deniselewis1120@gmail.com>
2023-11-02 14:02:20 -0700

    Display result of game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [userChoice, setUserChoice] = useState("");
   const [computerChoice, setComputerChoice] = useState("");
+  const [result, setResult] = useState("");
   const choices = ["rock", "paper", "scissors"];
 
   const handleClick = value => {
@@ -16,11 +17,32 @@ const App = () => {
     setComputerChoice(randomChoice);
   };
 
+  useEffect(() => {
+    switch(userChoice + computerChoice) {
+      case "rockpaper":
+      case "paperscissors":
+      case "scissorsrock":
+        setResult("You lose!");
+        break;
+      case "paperrock":
+      case "scissorspaper":
+      case "rockscissors":
+        setResult("You win!");
+         break;
+      case "rockrock":
+      case "paperpaper":
+      case "scissorsscissors":
+        setResult("It's a draw!");
+        break;
+    };
+  }, [computerChoice, userChoice]);
+
   return (
     <div>
       <h1>User choice is: {userChoice}</h1>
       <h1>Computer choice is: {computerChoice}</h1>
       {choices.map((choice, index) => <button key={index} onClick={() => handleClick(choice)}>{choice}</button>)}
+      <h1>{result}</h1>
     </div>
   );
 }
